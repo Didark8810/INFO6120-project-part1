4a2ed450b6a2427281f49edf00e3070290ae8b16
Didark8810 <diegobernal8810@hotmail.com>
2024-11-05 21:18:55 -0500

    init code

---
+++ src/database.java
@@ -0,0 +1,68 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class database {
+
+    private Connection connect() {
+        String url = "jdbc:sqlite:src/sakila.db"; 
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    //list all actors
+    public void selectAll(){
+        String sql = "SELECT first_name,last_name FROM actor";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            // loop through the result set
+            while (rs.next()) {
+                System.out.printf("%-10s %-10s%n", rs.getString("first_name"),rs.getString("last_name"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Insert new actors
+    public void insertRow(String firstName, String lastName)
+    {
+    	String SQL = "INSERT INTO actor (first_name,last_name) VALUES (?, ?)";      	
+    	try
+    	{
+    		Connection conn = this.connect();	
+    		PreparedStatement stmt = conn.prepareStatement(SQL); 	
+    		stmt.setString(1, firstName);
+    		stmt.setString(2, lastName);
+    		stmt.executeUpdate();
+    	}
+    	catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    	}
+    }
+    
+    //Delete actors by first name and last name
+    public void deleteRow(String firstName, String lastName)
+    {
+    	String SQL = "DELETE FROM actor WHERE first_name = '" +firstName+"' and last_name='"+lastName+"';" ;
+    	try
+    	{
+    		Connection conn = this.connect();
+    		Statement stmt = conn.createStatement();
+    		stmt.executeUpdate(SQL);
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+}
---
+++ src/mainfile.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class mainfile {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        String userCommand_firstName="";
+	        String userCommand_lastName ="";
+	        database data = new database();
+	        boolean exitProgram = false;
+	        while(!exitProgram) {
+		        System.out.print("Enter a command: ");
+		        String userCommand = scanner.nextLine();
+		        if(userCommand.toLowerCase().equals("add")) {
+		        	
+		        	System.out.print("Enter a first name: ");
+			        userCommand_firstName = scanner.nextLine();
+			        
+		        	System.out.print("Enter a last name: ");
+			        userCommand_lastName = scanner.nextLine();
+			        //add actor to the data base
+			        data.insertRow(userCommand_firstName,userCommand_lastName);
+		        }else if(userCommand.toLowerCase().equals("list")) {
+		        	//show all actors
+		        	data.selectAll();
+		        }
+		        else if(userCommand.toLowerCase().equals("remove") ) {
+		        	System.out.print("Enter a first name: ");
+			        userCommand_firstName = scanner.nextLine();
+
+		        	System.out.print("Enter a last name: ");
+			        userCommand_lastName = scanner.nextLine();
+			        //remove actor from the data base
+			        data.deleteRow(userCommand_firstName,userCommand_lastName);
+		        }
+		        else if(userCommand.toLowerCase().equals("exit")) {
+		        	//finish the program
+		        	exitProgram = true;
+		        	System.out.println("bye =)");		
+		        }
+		        else {
+		        	System.out.println("Invalid command");
+		        }
+	        }
+	        scanner.close();   
+	    }
+}
